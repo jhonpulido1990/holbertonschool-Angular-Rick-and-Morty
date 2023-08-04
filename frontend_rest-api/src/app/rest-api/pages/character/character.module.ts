3f259ded9da654367e4a154a6736dd49d00bf0c9
jhonpulido1990 <3825@holbertonstudents.com>
2023-08-04 12:30:10 -0500

    step 4

--- frontend_rest-api/src/app/rest-api/pages/character/character.module.ts
+++ frontend_rest-api/src/app/rest-api/pages/character/character.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { CharacterRoutingModule } from './character-routing.module';
 import { CharacterComponent } from './character.component';
 
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
   declarations: [CharacterComponent],
   imports: [
     CommonModule,
-    CharacterRoutingModule
+    CharacterRoutingModule,
+    SharedModule
   ]
 })
 export class CharacterModule { }
