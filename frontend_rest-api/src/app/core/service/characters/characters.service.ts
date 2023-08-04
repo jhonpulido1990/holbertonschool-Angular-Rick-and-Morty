284e3ae5903de7a62c00b1c31f260916251ac91f
jhonpulido1990 <3825@holbertonstudents.com>
2023-08-04 12:31:35 -0500

    fixed

--- frontend_rest-api/src/app/core/service/characters/characters.service.ts
+++ frontend_rest-api/src/app/core/service/characters/characters.service.ts
@@ -2,21 +2,31 @@ import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment.development';
 import { Observable } from 'rxjs';
-import { Caracters, CaractersAnime } from '../../interface/characters.interfaces';
+import {
+  AllCaracters,
+  Caracters,
+  CaractersAnime,
+} from '../../interface/characters.interfaces';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CharactersService {
-
   private readonly baseUrl = environment.apiUrl;
 
   http = inject(HttpClient);
 
   getNavbar(): Observable<Caracters[]> {
-    return this.http.get<Caracters[]>(this.baseUrl)
+    return this.http.get<Caracters[]>(this.baseUrl);
   }
   getCaracterHome(): Observable<CaractersAnime[]> {
-    return this.http.get<CaractersAnime[]>(`${this.baseUrl}/character/1,2,3,4,5,6,7,8,9,10`)
+    return this.http.get<CaractersAnime[]>(
+      `${this.baseUrl}/character/1,2,3,4,5,6,7,8,9,10`
+    );
+  }
+  getCaracter(data: string ): Observable<AllCaracters[]> {
+    return this.http.get<AllCaracters[]>(
+      `${this.baseUrl}/character/${data}`
+    );
   }
 }
--- frontend_rest-api/src/app/rest-api/pages/character/character.component.ts
+++ frontend_rest-api/src/app/rest-api/pages/character/character.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { AllCaracters } from 'src/app/core/interface/characters.interfaces';
+
+import { CharactersService } from 'src/app/core/service/characters/characters.service';
 
 @Component({
   selector: 'app-character',
   templateUrl: './character.component.html',
   styleUrls: ['./character.component.scss']
 })
-export class CharacterComponent {
+export class CharacterComponent implements OnInit {
+  dataService = inject(CharactersService);
+  data!:AllCaracters[]
+
+  ngOnInit(): void {
+    this.getAnimeAll()
+  }
+
+  getAnimeAll() {
+    this.dataService.getCaracter('')
+    .subscribe(res => {
+      this.data = res
+    })
+  }
 
 }
